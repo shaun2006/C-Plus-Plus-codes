ac338107086f2a28255d19ce4466e9ec43207503
Shaun2006 <116490490+shaun2006@users.noreply.github.com>
2025-07-10 23:44:08 +0530

    area of circle

---
+++ circle-area.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int r,a;
+  cout << "Enter radius of the circle: ";
+  cin >> r;
+  a = 3.14*r*r;
+  cout << "\n";
+  cout << "Area of the circle is ";
+  cout << a;
+  cout << "\n";
+}
