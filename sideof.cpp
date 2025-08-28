eba2cc849025d15163d3ab2ff27abd9ddea5aca2
Shaun2006 <116490490+shaun2006@users.noreply.github.com>
2025-08-28 21:54:21 +0530

    checks size of data types of c++
    prints out size of some basic data types like int, double, char, float, boolean

---
+++ sideof.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{   
+    /* side of each data type in c++
+    int
+    double
+    char
+    float
+    bool
+    */
+    cout << "size of int is = " << sizeof(int) << " bytes" << endl;
+    cout << "size of double is = " << sizeof(double) << " bytes" << endl;
+    cout << "size of char is = " << sizeof(char) << " bytes" << endl;
+    cout << "size of float is = " << sizeof(float) << " bytes" << endl;
+    cout << "size of bool is = " << sizeof(bool) << " bytes" << endl;
+}
